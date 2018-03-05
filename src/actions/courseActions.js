9dd2d060b71445275d4869352f4172d389ce4950
iceman69 <bmead92@gmail.com>
2018-03-05 17:26:52 -0500

    Updates from Monday

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -5,6 +5,15 @@ export function loadCoursesSuccess(courses){
     return {type: types.LOAD_COURSES_SUCCESS, courses};
 }
 
+export function createCourseSuccess(course){
+  return {type: types.CREATE_COURSE_SUCCESS, course};
+}
+
+export function updateCourseSuccess(course){
+  return {type: types.UPDATE_COURSE_SUCCESS, course};
+}
+
+//this is a thunk
 export function loadCourses() {
     return function(dispatch) {
         return courseApi.getAllCourses().then(courses => {
@@ -13,4 +22,15 @@ export function loadCourses() {
             throw(error);
         });
     };
+}
+
+export function saveCourse(course) {
+  return function (dispatch, getState) {
+    return courseApi.saveCourse(course).then(savedCourse => {
+      course.id ? dispatch(updateCourseSuccess(savedCourse)) :
+        dispatch(createCourseSuccess(savedCourse));
+    }).catch(error => {
+      throw(error);
+    });
+  };
 }
